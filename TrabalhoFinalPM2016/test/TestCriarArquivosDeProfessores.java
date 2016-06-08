fb6ae6bbda93cd8ea10330c6a45fea7c779fdd79
Gabriel-Ramos <gabrielramosprincipal@gmail.com>
2016-06-08 18:13:23 -0300

    Conserto

---
+++ TrabalhoFinalPM2016/test/TestCriarArquivosDeProfessores.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import trabalhofinalpm2016.CriarArquivosDeProfessores;
+
+/**
+ *
+ * @author Gabriel
+ */
+public class TestCriarArquivosDeProfessores {
+    
+    @Test
+    public void testeEscreverArquivoFinal(){
+
+
+  }
+    
+}
